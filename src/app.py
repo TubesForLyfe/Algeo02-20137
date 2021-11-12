05385e5f1dfffd56baa561829598643339241a0d
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-11-12 16:01:27 +0700

    Add upload image

--- src/app.py
+++ src/app.py
@@ -1,8 +1,36 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import os
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+app.config["IMAGE"] =  "C:/Users/willy/Tubes-Algeo_2_IF_2123/image"
+app.config["ALLOW"] =["PNG", "JPG", "JPEG", "GIF"]
+
+def allowed(filename):
+   if not "." in filename:
+      return False
+
+   format = filename.rsplit(".", 1)[1]
+   if format.upper() in app.config["ALLOW"]:
+      return True
+   else:
+      return False
+
+@app.route('/', methods=["GET","POST"])
+def main():
+   if request.method == "POST":
+      if request.files:
+         image = request.files["image"]
+         if not(allowed(image.filename)):
+            print("Image extension is not allowed")
+         else:
+            filename = secure_filename(image.filename)
+            if not(os.path.isdir(app.config["IMAGE"])):
+               os.makedirs(app.config["IMAGE"])
+            image.save(os.path.join(app.config["IMAGE"], filename))
+            print("Image saved")
+         return redirect(request.url)
    return render_template('gwf.html')
 
 if __name__ == '__main__':
