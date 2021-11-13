78a94d10d55063b863f0015f089cfae72835a9e9
Muhammad Gilang Ramadhan <80266785+gilanglahat22@users.noreply.github.com>
2021-11-13 20:53:40 +0700

    Update matriks.py

--- src/python/matriks.py
+++ src/python/matriks.py
@@ -3,198 +3,120 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import array
-from numpy.linalg import norm
-from random import normalvariate
 from math import sqrt
-from scipy.linalg import null_space
-double_epsilon = 1e-140
+from math import copysign
+
+# Operasi Yang Diperlukan
 
 
-def add(m1, m2):
-    return m1+m2
+def DiagonalMatriks(A):
+    return np.diagonal(A)
 
 
-def substract(m1, m2):
-    return m1-m2
+def transpose(A):
+    return np.transpose(A)
 
 
-def dot(m1, m2):
-    return np.dot(m1, m2)
+def array(A):
+    return np.array(A)
 
 
-def transpose(m):
-    return np.transpose(m)
+def Dot(A, B):
+    return np.dot(A, B)
 
 
-def det(m):
-    return np.linalg.det(m)
+def get_u(A, v, sigma):
+    u = []
+    for i in range(len(sigma)):
+        ui = (1/sigma[i])*Dot(A, v.T[i])
+        u.append(ui)
+
+    return transpose(array(u))
 
 
-def inv(m):
-    return np.linalg.inv(m)
-
-
-def normalize(m):
-    return m/np.linalg.norm(m)
-
-
-def swap_row(m, r1, r2):
-    m[[r1, r2]] = m[[r2, r1]]
-    return m
-
-
-def multiply(m, k, r1):
-    l = len(m[0])
-    for i in range(l):
-        m[r1][i] *= k
-    return m
-
-
-def add_multiply(m, k, r, rm):
-    l = len(m[0])
-    for i in range(l):
-        m[r][i] += k*m[rm][i]
-    return m
-
-
-def eq(a, b):
-    return abs(a-b) < double_epsilon
-
-
-def minimum(A, B):
-    if A >= B:
-        return B
-    else:
-        return A
-
-
-def multiply_matriks(A, B):
-    return np.matmul(A, B)
-
-
-def ukuranMatriks(A):
-    n, m = A.shape
-    return n, m
-
-
-def singularKiri(A):
-    return dot(A, transpose(A))
-
-
-def singularKanan(A):
-    return dot(transpose(A), A)
-
-
-def ArrayOfFloat(A):
-    return np.array(A, dtype=float)
-
-
-def CopyMatriks(A):
-    return A.copy()
-
-
-def Keluaran(A, B):
+def Outer(A, B):
     return np.outer(A, B)
 
 
-def SplitMatriks(A):
-    X, Y, Z = [array(i) for i in zip(*A)]
-    return X, Y, Z
+def Identity(A):
+    return np.identity(A)
 
 
-def nullspace(A):
-    D = []
-    NullSpace = null_space(A)
-    for i in range(len(NullSpace)):
-        D.extend(NullSpace[i])
-    return D
+def Norm(A):
+    return np.linalg.norm(A)
 
 
-def VektorAwal(ukuran):
-    '''
-    Initialize Vektor awal yang diambil secara random
-
-    '''
-    TidakNormalisasi = [0 for i in range(ukuran)]
-    VektorAwal = [0 for i in range(ukuran)]
-    sum = 0
-    for i in range(ukuran):
-        TidakNormalisasi[i] = normalvariate(0, 1)
-        sum += TidakNormalisasi[i] ** 2
-    Normalisasi = sqrt(sum)
-    for i in range(ukuran):
-        VektorAwal[i] = TidakNormalisasi[i]/Normalisasi
-    return VektorAwal
+def CopyMatriks(A):
+    return np.copy(A)
 
 
-def SVD_Awal(A, epsilon=1e-10):
-    '''
-    Menghitung iteration awal dari SVD dengan menggunakan Power Method
-
-    '''
-    A = ArrayOfFloat(A)
-    N, M = ukuranMatriks(A)
-    x = VektorAwal(minimum(N, M))  # set x sebagai vektor awal yang sembarang
-    count = 0
-    V_Awal = x
-    if N <= M:
-        B = singularKiri(A)
-    else:
-        B = singularKanan(A)
-    V_Akhir = V_Awal
-    V_Awal = dot(B, V_Akhir)
-    V_Awal = V_Awal / norm(V_Awal)
-    count += 1
-    while (abs(dot(V_Awal, V_Akhir)) + epsilon <= 1):
-        print("-------------------------------------------------------------")
-        print("LOADING")
-        print("-------------------------------------------------------------")
-        print("Mohon Tunggu")
-        V_Akhir = V_Awal
-        V_Awal = dot(B, V_Akhir)
-        V_Awal = V_Awal / norm(V_Awal)
-        count += 1
-    return V_Awal
+def UkuranMatriks(A):
+    return np.shape(A)
 
 
-def SVD(A, epsilon=1e-10):
-    '''
-    Menghitung Next iteration dari SVD dengan menggunakan Power Method
+def Zero_Like(A):
+    return np.zeros_like(A)
 
-    '''
-    N, M = ukuranMatriks(A)
-    Tampungan_SVD = []
-    Min_Ukuran = minimum(N, M)
-    increment = 0
-    while (increment < Min_Ukuran):
-        MatrixClone = CopyMatriks(A)
-        for singularValue, U, V in Tampungan_SVD[0:increment]:
-            MatrixClone -= singularValue * Keluaran(U, V)
-        if N > M:
-            V = SVD_Awal(MatrixClone, epsilon=epsilon)
-            U_TidakNormalized = dot(A, V)
-            sigma = norm(U_TidakNormalized)
-            U = U_TidakNormalized / sigma
-        else:  # jika M >= N
-            U = SVD_Awal(MatrixClone, epsilon=epsilon)
-            V_TidakNormalized = dot(transpose(A), U)
-            sigma = norm(V_TidakNormalized)
-            V = V_TidakNormalized / sigma
-        Tampungan_SVD.append((sigma, U, V))
-        increment += 1
-    singularValues, U_Singular, V_Singular = SplitMatriks(Tampungan_SVD)
+# Code Untuk Mencari SVD
 
-    C = []
-    C.extend(nullspace(multiply_matriks(transpose(A), A)))
-    V_SingularFix = [[] for i in range(len(transpose(V_Singular)))]
-    for i in range(len(transpose(V_Singular))):
-        for j in range(len(transpose(V_Singular)[i])):
-            V_SingularFix[i].append(transpose(V_Singular)[i][j])
-    for i in range(len(V_SingularFix)):
-        V_SingularFix[i].append(C[i])
-    V_SingularFix1 = np.array(V_SingularFix)
 
-    return U_Singular, singularValues, V_SingularFix1
+def Refleksi_HouseHolder(A):
+    (Baris, Kolom) = UkuranMatriks(A)
+    Q = Identity(Baris)
+    R = CopyMatriks(A)
+
+    for i in range(Baris - 1):
+        x = R[i:, i]
+
+        e = Zero_Like(x)
+        e[0] = copysign(Norm(A), -A[i, i])
+        u = x + e
+
+        v = u / Norm(u)
+
+        Q_i = Identity(Baris)
+        Q_i[i:, i:] -= 2.0 * Outer(v, v)
+
+        R = Dot(Q_i, R)
+        Q = Dot(Q, Q_i.T)
+
+    return (Q, R)
+
+
+def qr_eigen(M, maxIter):
+    """
+    Menghitung nilai Sigma dan V
+
+    """
+    # Initialize empty array to store eigenvalues
+    A = []
+
+    V = np.eye(M.shape[0])
+
+    # Append input matrix M to A
+    A.append(None)
+    A.append(M)
+
+    # lakukan iterasi sebanyak maxIter untuk menghitung eigenvalue and eigenvector
+    # Gunakan QR houseHolder Reflection
+    for k in range(maxIter):
+        A[0] = A[1]
+        q, R = Refleksi_HouseHolder(A[0])
+        A[1] = Dot(R, q)
+        V = Dot(V, q)
+    SingularValues = []
+    for i in DiagonalMatriks(A[1]):
+        if i > 0:
+            SingularValues.append(sqrt(i))
+    Sigma = array(SingularValues)
+    return Sigma, V
+
+
+def SVD(A, K):
+    Sigma, V = qr_eigen(Dot(A.T, A), K)
+    U = get_u(A, V, Sigma)
+    V_singular = np.negative(V)
+    return np.negative(U), Sigma, V_singular.T
 
 
 NAME = input('MASUKKAN NAMA FILE YANG INGIN DICOMPRESS : ')
@@ -212,7 +134,7 @@ def show_images(img_name):
     plt.show()
 
 
-show_images('GWF KOMPRESS')
+show_images("GWF KOMPRESS")
 compressed_image = None
 
 
@@ -224,9 +146,9 @@ def Kompress_image(Nama_image, k):
     g = img[:, :, 1]
     b = img[:, :, 2]
     print("compressing...")
-    ur, sr, vr = SVD(r)
-    ug, sg, vg = SVD(g)
-    ub, sb, vb = SVD(b)
+    ur, sr, vr = SVD(r, k)
+    ug, sg, vg = SVD(g, k)
+    ub, sb, vb = SVD(b, k)
     rr = np.dot(ur[:, :k], np.dot(np.diag(sr[:k]), vr[:k, :]))
     rg = np.dot(ug[:, :k], np.dot(np.diag(sg[:k]), vg[:k, :]))
     rb = np.dot(ub[:, :k], np.dot(np.diag(sb[:k]), vb[:k, :]))
